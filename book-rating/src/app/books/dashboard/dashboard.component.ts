aad58d3ebbe681ad4f982beeca974c3b89a7b348
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-05 14:47:52 +0200

    Standalone Components

---
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule /* BlubbComponent */],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+
+}
