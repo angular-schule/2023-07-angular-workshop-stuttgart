129ce51bfb6cfe31e3663bbb6fad16496e6693a8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-07 13:40:49 +0200

    implementing test for BookRatingService

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class BookRatingService {
 
+  readonly minRating = 1;
+  readonly maxRating = 5;
+
   rateUp(book: Book): Book {
-    return book; // TODO: real implementation
+    if (book.rating >= this.maxRating) {
+      return book;
+    }
+
+    return {
+      ...book,
+      rating: book.rating + 1
+    }
   }
 
   rateDown(book: Book): Book {
-    return book; // TODO: real implementation
+    const { isbn, title, description, rating: oldRating } = book;
+    const rating = Math.max(oldRating - 1, this.minRating);
+
+    return {
+      isbn,
+      title,
+      description,
+      rating
+    };
   }
 }
