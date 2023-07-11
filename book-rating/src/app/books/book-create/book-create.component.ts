1aecb1b3461d9dd901cbd681171c9b19d927c758
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-11 10:20:37 +0200

    TODO: submitForm()

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-book-create',
@@ -29,4 +30,27 @@ export class BookCreateComponent {
   });
 
   c = this.bookForm.controls;
+
+  isInvalid(control: FormControl) {
+    return control.invalid && control.touched;
+  }
+
+  hasError(control: FormControl, errorCode: 'minlength' | 'required') {
+    return control.hasError(errorCode) && control.touched;
+  }
+
+  submitForm() {
+    const newBook: Book = {
+      ...this.bookForm.getRawValue(),
+      rating: 1
+    };
+
+    // TODO:
+    // 1. Erzeuge ein Event mit dem Namen create
+    // 2. Versende das neue Buch per Event
+    // 3. Subscribe dich im Dashboard auf das Event
+    // 4. Füge das neue Buch dem Buch-Array hinzu (immutable!)
+
+    this.bookForm.reset();
+  }
 }
