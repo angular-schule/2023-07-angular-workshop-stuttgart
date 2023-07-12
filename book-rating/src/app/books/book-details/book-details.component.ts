fc3c0d25c0b80d50e142105fb4b0596566097e40
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-12 14:37:54 +0200

    rxjs: one-time error handling

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
 import { BookStoreService } from '../shared/book-store.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { concatMap, map, mergeMap } from 'rxjs';
+import { catchError, concatMap, map, mergeMap, of, switchMap } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [CommonModule, RouterLink, AsyncPipe],
+  imports: [CommonModule, RouterLink, AsyncPipe, NgIf],
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
@@ -16,7 +17,10 @@ export class BookDetailsComponent {
   bookStore = inject(BookStoreService);
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(param => param.get('isbn') || ''),
-    concatMap(isbn => this.bookStore.getSingleBook(isbn))
+    switchMap(isbn => this.bookStore.getSingleBook(isbn)),
+    catchError((err: HttpErrorResponse) => of({
+      title: 'FEHLER',
+      description: err.message
+    }))
   )
-
 }
