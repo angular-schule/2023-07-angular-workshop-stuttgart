3df2dc623852a8232bda6cb315b9a2897e70f107
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-12 11:55:02 +0200

    rxjs: unsubscribe ngOnDestroy

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -11,6 +11,8 @@ export class UnsubscribeComponent implements OnDestroy {
 
   logStream$ = new ReplaySubject<string | number>();
 
+  subscription: Subscription;
+
   /**
    * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
    * Navigiere zurück auf die Startseite und beobachte die Console:
@@ -24,11 +26,11 @@ export class UnsubscribeComponent implements OnDestroy {
   constructor() {
     const interval$ = timer(0, 1000);
 
-    interval$.pipe(
+    this.subscription = interval$.pipe(
 
       /******************************/
 
-      
+
       /******************************/
 
     ).subscribe({
@@ -39,6 +41,7 @@ export class UnsubscribeComponent implements OnDestroy {
   }
 
   ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   log(msg: string | number) {
