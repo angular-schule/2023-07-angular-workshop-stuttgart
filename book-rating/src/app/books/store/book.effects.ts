c3de9ad59b50ad11b2d19ac45d1bbbb492988320
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-12 15:32:54 +0200

    ngrx: effects

--- book-rating/src/app/books/store/book.effects.ts
+++ book-rating/src/app/books/store/book.effects.ts
@@ -1,22 +1,25 @@
 import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, concatMap } from 'rxjs/operators';
+import { catchError, map, concatMap, switchMap, retry } from 'rxjs/operators';
 import { Observable, EMPTY, of } from 'rxjs';
 import { BookActions } from './book.actions';
+import { BookStoreService } from '../shared/book-store.service';
 
 
 @Injectable()
 export class BookEffects {
 
+  bookStore = inject(BookStoreService);
+
   loadBooks$ = createEffect(() => {
     return inject(Actions).pipe(
 
       ofType(BookActions.loadBooks),
-      concatMap(() =>
-        /** An EMPTY observable only emits completion. Replace with your own observable API request */
-        EMPTY.pipe(
+      switchMap(() =>
+        this.bookStore.getAllBooks().pipe(
+          retry(3),
           map(data => BookActions.loadBooksSuccess({ data })),
-          catchError(error => of(BookActions.loadBooksFailure({ error }))))
+          catchError(error => of(BookActions.loadBooksFailure({ error })))),
       )
     );
   });
